e1a78e81c7eb4dbc5fbf6fccfdf6910889df1160
Rafael Luiz <rafelluiz@gmail.com>
2021-03-08 15:17:24 -0300

    Adding shoulda matcher for user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  let(:user) { build(:user) }
 
-  it {expect(@user).to respond_to(:email) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value('rafael@gmail.com').for(:email)}
 end
